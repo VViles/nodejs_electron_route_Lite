222f516517e9f859fe6a522e4f193b61b63e6531
oxiaom <244255055@qq.com>
2018-04-28 16:02:02 +0800

    oxiaom

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const electron = require('electron');
 const app = electron.app;
-
 const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const url = require('url');
 
 var mainWindows;
+ 
 app.on('ready',function(){
-    mainWindows = new BrowserWindow({width:1024,height:768,backgroundColor:'#2e2c29'});
-    mainWindows.loadURL('https://github.com');
+    mainWindows = new BrowserWindow({width:370,height:600,backgroundColor:'#eee',autoHideMenuBar:true});
+    mainWindows.loadURL(url.format({
+        pathname:path.join(__dirname,'index.html'),
+        protocol :'file',
+        slashes:true
+        }));
+        mainWindows.webContents.openDevTools()
+        mainWindows.on('close',function(){
+            win= null;
+        });    
+    
 
 });
+ 
+ 
 
